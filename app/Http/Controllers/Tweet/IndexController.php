684b53618ecf67b9e1f4b2bedbca1cbc8b1cfe28
phucnguyen2998 <phucnguyen260698@gmail.com>
2024-01-13 14:54:44 +0900

    first commit

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+use App\Services\TweetService;
+
+class IndexController extends Controller
+{
+    public function __invoke(Request $request, TweetService $tweetService)
+    {
+        $tweetService = new TweetService();
+        $tweets = $tweetService->getTweets();
+        return view('tweet.index')
+            ->with('tweets', $tweets);
+    }
+}
